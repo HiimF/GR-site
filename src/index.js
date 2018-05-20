93f7d895268cf908fe5a600e042641577e6e3073
HiimF <flora@grama.cc>
2018-05-20 12:16:34 -0300

    js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ import './scss/style.scss';
 
 
 
+;(function() {
+  $( '#header .menu li a' ).on( 'click', function( e ) {
+    e.preventDefault();
+    console.log(1);
+
+    var selector = $( e.currentTarget ).attr( 'href' );
+    var position = $( selector ).offset().top;
+
+    var scroll = anime({
+      targets: 'html, body',
+      scrollTop: position,
+      duration: 1000,
+      easing: 'easeOutExpo'
+    });
+  });
+
+})();
